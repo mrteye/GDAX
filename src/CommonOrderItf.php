6852362f101be5299bbdde26a2f44f0d6917a00e
Tyler Gillispie <mrteye@gmail.com>
2017-08-15 11:12:31 -0700

    Update documentation for CommonOrderItf

--- src/CommonOrderItf.php
+++ src/CommonOrderItf.php
@@ -1,26 +1,58 @@
 <?php
+/**
+ * Common Order Interface
+ */
 namespace mrteye\Gdax;
 
+/**
+ *  The CommonOrder interface holds the common properties for the three
+ * order types: limit, market, and stop.
+ */
 abstract class CommonOrderItf extends Hydration {
-  /** 
-   *  client_oid:     [optional] UUID for client reference only
-   *  type:           [optional] limit, market, stop; default is limit
-   *  side:           buy or sell
-   *  product_id:     Valid Product ID
-   *  stp:            [optional]  self-trade prevention flag
+  /**
+   * [optional] UUID from client; reference only
+   * @var string
    */
   public $client_oid;
+
+  /**
+   * limit, market, stop
+   * @var string
+   */
   protected $type = 'limit';
+
+  /**
+   * buy or sell
+   * @var string
+   */
   public $side;
+
+  /**
+   * Valid Product ID
+   * @var string
+   */
   public $product_id;
-  public $stp = 'dc'; // dc, co, cn, cb
+
+  /**
+   * self-trade prevention flag: dc, co, cn, cb
+   * @var string
+   */ 
+  public $stp = 'dc';
 
   /**
    *      Margin Parameters
-   *  overdraft_enabled: overdraft flag
-   *  funding_amount: amount of funding to be provided for the order
+   */
+
+  /**
+   * overdraft flag
+   *  @var string
    */
   public $overdraft_enabled = false;
+
+  /**
+   * funding amount to be provided for the order
+   * @var string
+   */
   public $funding_amount = "";
 }
 
